a20a007976db996ab4ccdc4f04bb0b1798af8235
sebitse <137815242+sebitse@users.noreply.github.com>
2024-06-29 15:16:03 +0300

    requirements added

---
+++ check_packages.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def check_packages(requirements_file):
+    try:
+        with open(requirements_file, 'r') as file:
+            packages = [line.strip().split('==')[0] for line in file if line.strip() and not line.startswith('#')]
+        
+        for package in packages:
+            print(f"\nChecking package: {package}")
+            subprocess.run(['pip', 'show', package], check=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    requirements_file = 'requirements.txt'  # Change this if your file has a different name
+    check_packages(requirements_file)
+
